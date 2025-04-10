0d896e5aa42eb688b81ddcf82cb6436c4feaa89f
William Felipe <will25felipe@gmail.com>
2025-04-10 16:41:25 -0300

    fix: use contants in logs

--- src/log/Log.java
+++ src/log/Log.java
@@ -7,6 +7,10 @@ import veiculo.Veiculo;
 
 public class Log {
 
+    private static String FABRICA_LOG_FILE = "fabricaLog.txt";
+    private static String LOJA_LOG_FILE = "lojaLog.txt";
+    private static String CLIENTE_LOG_FILE = "clienteLog.txt";
+
     private static synchronized void writeLog(String file, String content) {
         try {
             FileWriter arquivoLog = new FileWriter(file, true);
@@ -23,30 +27,30 @@ public class Log {
     public static synchronized void registroProducaoFabrica(Veiculo v) {
         String content = "Log Producao Fabrica: " + v;
         System.out.println(content);
-        writeLog("fabricaLog.txt", content);
+        writeLog(FABRICA_LOG_FILE, content);
     }
 
     public static synchronized void registroVendaFabrica(Veiculo v, String loja, int posLoja) {
         String content = "Log Venda Fabrica: " + v + " -> " + loja + " (pos=" + posLoja + ")";
         System.out.println(content);
-        writeLog("fabricaLog.txt", content);
+        writeLog(FABRICA_LOG_FILE, content);
     }
 
     public static synchronized void registroRecebimentoLoja(Veiculo v, String loja, int posLoja) {
         String content = "Log Recebimento Loja: " + v + " -> " + loja + " (pos=" + posLoja + ")";
         System.out.println(content);
-        writeLog("lojaLog.txt", content);
+        writeLog(LOJA_LOG_FILE, content);
     }
     public static synchronized void registroVendaLoja(Veiculo v, String loja, int cliente) {
         String content = "Log Venda Loja: " + v + " -> Cliente " + cliente + " (" + loja + ")";
         System.out.println(content);
-        writeLog("lojaLog.txt", content);
+        writeLog(LOJA_LOG_FILE, content);
     }
 
     public static synchronized void registroGaragemCliente(Veiculo v, int cliente, int posGaragem) {
         String content = "Log Garagem Cliente " + cliente + ": " + v + " (pos=" + posGaragem + ")";
         System.out.println(content);
-        writeLog("clienteLog.txt", content);
+        writeLog(CLIENTE_LOG_FILE, content);
     }
 
 
